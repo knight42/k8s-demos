08a32583f48a93c2e9b1e8b2274ade58c410e890
knight42 <anonymousknight96@gmail.com>
2019-02-14 17:01:49 +0800

    feat: add kubectl-rm

---
+++ cmd/kubectl-rm/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/knight42/k8s-tools/pkg/rm"
+)
+
+func main() {
+	_ = rm.NewCmdRm().Execute()
+}
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/knight42/k8s-tools/cmd"
-
-func main() {
-	cmd.Execute()
-}
